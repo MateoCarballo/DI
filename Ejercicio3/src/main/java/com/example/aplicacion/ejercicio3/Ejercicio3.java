0684d2d9382c47eb6241f5487b4694e2d70e5402
Mateo <mateocarballo2@gmail.com>
2024-09-20 17:33:40 +0200

    Acabada tarea 3

---
+++ Ejercicio3/src/main/java/com/example/aplicacion/ejercicio3/Ejercicio3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.aplicacion.ejercicio3;
+
+/**
+ *
+ * @author a22mateoca
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
