45d05fc65bd59d5691a6d58b759f42770d6d8074
Mateo <mateocarballo2@gmail.com>
2024-11-08 18:22:02 +0100

    Acabada clase del 2024-11-08

---
+++ PostDavid/Border_Layout/src/main/java/Layouts/Card_Layout/MyCardLayout.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Layouts.Card_Layout;
+
+import java.awt.Button;
+import java.awt.CardLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author a22mateoca
+ */
+public class MyCardLayout extends JFrame{
+    
+   public MyCardLayout(){
+       
+       setTitle("Ejemplo CardLayout");
+       setLayout(new CardLayout());
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       add(new Button("1"));
+       add(new Button("2"));
+       setVisible(true);
+       setSize(500,500);
+       
+       setLocationRelativeTo(null);
+   } 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+       
+       MyCardLayout frame = new MyCardLayout();
+       
+        }
+}
+
+
