0e6bb189759c7ead8661a59e56b55a9cf3e47ae8
Mateo <mateocarballo2@gmail.com>
2025-02-07 18:13:36 +0100

    Añadida tarea 9 completada

---
+++ Trimestre 2/Tarea_9_MenuChooser/src/main/java/com/example/aplicacion/tarea_9_menuchooser/Tarea_9_MenuChooser.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.aplicacion.tarea_9_menuchooser;
+
+/**
+ *
+ * @author a22mateoca
+ */
+public class Tarea_9_MenuChooser {
+
+    public static void main(String[] args) {
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.setVisible(true);
+    }        
+
+}
