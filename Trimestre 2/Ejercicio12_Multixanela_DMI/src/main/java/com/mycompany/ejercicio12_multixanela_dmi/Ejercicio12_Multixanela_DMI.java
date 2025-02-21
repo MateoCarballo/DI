40e93f60c8094338329dfa2b97245ea903a0bd6c
MateoPortatil <mateocarballo2@gmail.com>
2025-02-21 18:40:11 +0100

    A medias con tarea 12

---
+++ Trimestre 2/Ejercicio12_Multixanela_DMI/src/main/java/com/mycompany/ejercicio12_multixanela_dmi/Ejercicio12_Multixanela_DMI.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio12_multixanela_dmi;
+import com.mycompany.ejercicio12_multixanela_dmi.vista.VentanaContenedora;
+
+/**
+ *
+ * @author mateo
+ */
+public class Ejercicio12_Multixanela_DMI {
+
+    public static void main(String[] args) {
+        VentanaContenedora vc = new VentanaContenedora();
+        vc.setVisible(true);
+    }
+}
